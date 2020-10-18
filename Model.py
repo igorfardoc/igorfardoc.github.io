9756be9af7c00d77c55c73e37f85a0f1ed0bbc80
igorfardoc <igor.mirim@yandex.ru>
2020-10-18 14:58:43 +0000

    Fully working version with confirmed export to JS

--- Model.py
+++ Model.py
@@ -5,7 +5,7 @@ K = tf.keras.backend
 
 def get_encoder(voc, emb_size, hid_size):
     emb = L.Embedding(len(voc), emb_size)
-    rnn = L.GRU(units=hid_size, return_sequences=True, return_state=False)
+    rnn = L.GRU(units=hid_size, return_sequences=True, return_state=False, reset_after=False)
 
     input_tokens = L.Input([None], dtype='int32')
     input_embs = emb(input_tokens)
@@ -17,9 +17,11 @@ def get_encoder(voc, emb_size, hid_size):
 def get_decoder_step(voc, emb_size, hid_size):
     # create layers
     emb = L.Embedding(len(voc), emb_size)
-    rnn = L.GRU(units=hid_size, return_sequences=False, return_state=True)
+    rnn = L.GRU(units=hid_size, return_sequences=False, return_state=True, reset_after=False)
     token_prob = L.Dense(units=len(voc), activation='softmax')
-    attention_first = L.Dense(units=123, activation='tanh')
+    
+    attention_from_enc = L.Dense(units=hid_size)
+    attention_from_dec = L.Dense(units=hid_size)
     attention_second = L.Dense(units=1)
     
 
@@ -32,21 +34,24 @@ def get_decoder_step(voc, emb_size, hid_size):
 
     
     # apply attention
-    length = K.shape(states)[1]
-    batch_size = K.shape(states)[0]
-    dec_states = K.tile(K.reshape(prev_state, [batch_size, 1, -1]), [1, length, 1])
-    concat = K.concatenate([states, dec_states], axis=2)
-    concat.set_shape([None, None, 2 * hid_size])
     
-    after_first = attention_first(concat) # -> [batch_size, SIZE, hid_size]
-    after_second = attention_second(after_first) # -> [batch_size, SIZE, 1]
-    state_probs = K.softmax(after_second, axis=1) # -> [batch_size, SIZE, 1]
-    summary_state = K.sum(states * state_probs, axis=1)
+    enc_part = attention_from_enc(states)        # [batch_size, length, hid_size]
+    dec_part = attention_from_dec(prev_state)    # [batch_size, hid_size]
+    dec_part = L.Reshape([1, hid_size])(dec_part)      # [batch_size, 1, hid_size]
+    
+    after_first = L.Add()([enc_part, dec_part])  # [batch_size, length, hid_size]
+    after_first = L.Activation('tanh')(after_first)
+    
+    after_second = attention_second(after_first) # -> [batch_size, length, 1]
+    after_second = L.Reshape([-1])(after_second) # -> [batch_size, length]
+    
+    state_probs = L.Softmax(axis=1)(after_second) # -> [batch_size, length]
+    
+    summary_state = L.Dot(axes=(1, 1))([states, state_probs])  # [batch, hid_size]
     # end of attention
-    
     token_embs = emb(prev_tokens)  # [batch_size, emb_size]
-    rnn_inputs = K.expand_dims(K.concatenate([token_embs, summary_state], axis=1), axis=1)
-    
+    rnn_inputs = L.Concatenate(axis=1)([token_embs, summary_state])
+    rnn_inputs = L.Reshape([1, hid_size + emb_size])(rnn_inputs) # [batch_size, 1, hid_size + emb_size]
     new_state, _ = rnn(rnn_inputs, prev_state)
     # ^-- [batch_size, hid_size]
 
